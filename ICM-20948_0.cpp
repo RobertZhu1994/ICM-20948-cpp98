e77d6d2f4294aad30af99f6e694b1648fc3fe609
chisl <info@chisl.io>
2018-01-18 21:20:27 +0100

    update

--- ICM-20948_0.cpp
+++ ICM-20948_0.cpp
@@ -2,12 +2,17 @@
  * name:        ICM-20948
  * description: Low power 9-axis MotionTracking device that is ideally suited for Smartphones, Tablets, Wearable Sensors, and IoT applications.
  * manuf:       TDK Invensense
- * version:     0.1
+ * version:     Version 0.1
  * url:         https://www.invensense.com/wp-content/uploads/2016/06/DS-000189-ICM-20948-v1.3.pdf
  * date:        2017-10-18
  * author       https://chisl.io/
  * file:        ICM-20948_0.cpp
  */
+
+/*                                                                                                       *
+ *                                   THIS FILE IS AUTOMATICALLY CREATED                                  *
+ *                                    D O     N O T     M O D I F Y  !                                   *
+ *                                                                                                       */
 
 #include "ICM-20948_0.hpp"
 
